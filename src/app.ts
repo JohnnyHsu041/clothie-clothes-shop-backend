8769833c456713f6752e64796d0bea183af4a670
Johnny Hsu <a209328641@gmail.com>
2022-10-12 14:54:32 +0800

    created HttpError class. Built mongodb connection with mongoose

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ dotenv.config();
 
 import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
+import Mongoose from "mongoose";
+
+import HttpError from "./models/http-error";
 
 const app = express();
 
@@ -19,5 +22,25 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     next();
 });
 
-app.listen(process.env.PORT);
-console.log("works!");
+app.use((req: Request, res: Response, next: NextFunction) => {
+    return next(new HttpError("Could not find the route.", 404));
+});
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    res.status(err.code || 500).json({
+        message: err.message || "An unknown error occurred.",
+    });
+});
+
+Mongoose.connect(
+    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ezakmlr.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+)
+    .then(() => {
+        app.listen(process.env.PORT);
+        console.log("Connected to the database.");
+    })
+    .catch((err) => console.log(err));
