223d88f60ca2cc96c3b66b5e208b77988076e31a
Johnny Hsu <a209328641@gmail.com>
2022-10-28 14:44:36 +0800

    completed all product routes

--- src/controllers/products-controllers.ts
+++ src/controllers/products-controllers.ts
@@ -25,11 +25,11 @@ export const getAllProducts: RequestHandler = async (req, res, next) => {
         return next(new HttpError("No product found", 404));
     }
 
-    products.map((product) => product.toObject({ getters: true }));
-
     res.status(200).json({
         message: "Fetched all products data.",
-        products,
+        products: products.map((product) =>
+            product.toObject({ getters: true })
+        ),
     });
 };
 
@@ -46,11 +46,11 @@ export const getNewInProducts: RequestHandler = async (req, res, next) => {
         return next(new HttpError("No new-in product found", 404));
     }
 
-    products.map((product) => product.toObject({ getters: true }));
-
     res.status(200).json({
         message: "Fetched all new-in products data.",
-        products,
+        products: products.map((product) =>
+            product.toObject({ getters: true })
+        ),
     });
 };
 
@@ -69,11 +69,52 @@ export const getFeaturedProducts: RequestHandler = async (req, res, next) => {
         return next(new HttpError("No featured product found", 404));
     }
 
-    products.map((product) => product.toObject({ getters: true }));
-
     res.status(200).json({
         message: "Fetched all featured products data.",
-        products,
+        products: products.map((product) =>
+            product.toObject({ getters: true })
+        ),
+    });
+};
+
+export const getAllAccessories: RequestHandler = async (req, res, next) => {
+    let products;
+
+    try {
+        products = await ProductSchema.find({ type: "accs" }).exec();
+    } catch (err) {
+        return next(new HttpError("Failed to fetch accessories data", 500));
+    }
+
+    if (!products || products.length === 0) {
+        return next(new HttpError("No accessory found", 404));
+    }
+
+    res.status(200).json({
+        message: "Fetched all accessories data.",
+        products: products.map((product) =>
+            product.toObject({ getters: true })
+        ),
+    });
+};
+
+export const getProductById: RequestHandler = async (req, res, next) => {
+    const productId = req.params.pid;
+    let product;
+
+    try {
+        product = await ProductSchema.find({ _id: productId }).exec();
+    } catch (err) {
+        return next(new HttpError("Failed to fetch product data by Id", 500));
+    }
+
+    if (!product || product.length === 0) {
+        return next(new HttpError("No product found", 404));
+    }
+
+    res.status(200).json({
+        message: "Fetched the product data.",
+        product: product.map((product) => product.toObject({ getters: true })),
     });
 };
 
@@ -100,6 +141,37 @@ export const createProduct: RequestHandler = async (req, res, next) => {
     res.status(201).json({
         message: "created the product.",
         product: createdProduct.toObject({ getters: true }),
+    });
+};
+
+export const updateProduct: RequestHandler = async (req, res, next) => {
+    const productId = req.params.pid;
+
+    const { name: updatedName, price: updatedPrice } = req.body;
+
+    let product;
+    try {
+        product = await ProductSchema.findById(productId).exec();
+    } catch (err) {
+        return next(new HttpError("Failed to fetch the product data.", 500));
+    }
+
+    if (!product) {
+        return next(new HttpError("Product does not exist.", 404));
+    }
+
+    product.name = updatedName;
+    product.price = updatedPrice;
+
+    try {
+        await product.save();
+    } catch (err) {
+        return next(new HttpError("Failed to update the product.", 500));
+    }
+
+    res.status(200).json({
+        message: "Updated the product.",
+        product: product.toObject({ getters: true }),
     });
 };
 
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -2,9 +2,12 @@ import { Router } from "express";
 import {
     createProduct,
     deleteProduct,
+    getAllAccessories,
     getAllProducts,
     getFeaturedProducts,
     getNewInProducts,
+    getProductById,
+    updateProduct,
 } from "../controllers/products-controllers";
 
 const router = Router();
@@ -15,7 +18,13 @@ router.get("/newIn", getNewInProducts);
 
 router.get("/featured", getFeaturedProducts);
 
+router.get("/accs", getAllAccessories);
+
+router.get("/:pid", getProductById);
+
 router.post("/", createProduct);
+
+router.patch("/:pid", updateProduct);
 
 router.delete("/:pid", deleteProduct);
 
